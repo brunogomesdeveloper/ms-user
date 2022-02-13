eb740556078a0b4471eeb86d2f146a66fac1752c
Bruno Gomes <brunogomesdeveloper@gmail.com>
2022-02-13 16:34:04 -0300

    fix: add methods

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -17,15 +17,21 @@ export class UserService {
     return this.userRepository.findAll<User>();
   }
 
-  // findOne(id: number) {
-  //   return `This action returns a #${id} user`;
-  // }
+  findOne(id: number) {
+    return this.userRepository.findOne<User>({
+      where: { id: id },
+    });
+  }
 
-  // update(id: number, updateUserDto: UpdateUserDto) {
-  //   return `This action updates a #${id} user`;
-  // }
+  update(id: number, user: User) {
+    return this.userRepository.update<User>(user, {
+      where: { id: id },
+    });
+  }
 
-  // remove(id: number) {
-  //   return `This action removes a #${id} user`;
-  // }
+  remove(id: number) {
+    return this.userRepository.destroy<User>({
+      where: { id: id },
+    });
+  }
 }
